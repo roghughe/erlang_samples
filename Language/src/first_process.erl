629de8ff0092c4dd4ed42ccbc6edbd7621ea3d06
Roger Hughes <me@rogerhughes.co.uk>
2013-07-22 12:35:40 +0100

    Added more process examples

--- Language/src/first_process.erl
+++ Language/src/first_process.erl
@@ -7,7 +7,8 @@
 %% ====================================================================
 %% This is a first set of examples using spawn
 %% ====================================================================
--export([run/0,out/0,out2/0,print_this/1,hello_process/0,do_link1/0,do_link2/0,chain/1]).
+-export([run/0,out/0,out2/0,print_this/1,hello_process/0,do_link1/0,do_link2/0,chain/1,monitor_you/0,
+		 my_registered/0,is_init_process_running/0,is_init_process_running2/0,my_register/0]).
 
 %% Spawn a ping/0 process and send it a message
 run() ->
@@ -122,4 +123,96 @@ chain(N) ->
 	receive
 		_ -> ok
 	end.
+
+%% An example of a monitor. A monitor gets a message when a child process dies, but doesn't die itself
+%% @see http://www.erlang.org/doc/man/erlang.html#monitor-2
+%% Also demonstrates message selection using pattern matching
+monitor_you() ->
+	Pid = spawn(fun myproc/0),
+	Ref = monitor(process,Pid),
+	receive
+		% Match the incoming message to a pattern - 
+		% this tuple is the message sent when the myproc process dies.
+		{'DOWN', MessageRef, process, _Object, _Info} when MessageRef =:= Ref -> Ref;
+		
+		% Pattern2 when Guard2 -> body2;
+		% :
+		% PatternN when GuardN -> bodyN;
+		
+		% Message doesn't match any patterns, so remove it and exit (optional, obviously)
+		Default -> Default
+	end.
+
+
+%% Get hold of a list of all the requstered processes
+my_registered() ->
+	registered().
+
+
+%% Return true if the system process 'init' is running
+%% There is no list.contains(value) in erlang, so filtre the list and check the size of the result
+%% Should return true as the init process is a system process that's usually running
+is_init_process_running() ->
+	Processes = registered(),
+	Pred = fun(X) -> X =:= init end,
+	Length = length(lists:filter(Pred, Processes)),
+	if 
+		Length == 0 -> false;
+		Length == 1 -> true;
+		Length > 1 -> throw({oops,"Muliple init processes"})
+	end.
+		 
+%% Return true if the system process 'init' is running
+%% There is no list.contains(value) in erlang, so use a list comprehension to parse the list lookinh for 'init'
+%% Should return true as the init process is a system process that's usually running
+is_init_process_running2() ->
+	
+	Result = [ X || X <- registered(), X =:= init],
+	
+	Length = length(Result),
+	if 
+		Length == 0 -> false;
+		Length == 1 -> true;
+		Length > 1 -> throw({oops,"Muliple init processes"})
+	end.
+		 
+%% A generic is_process running function
+is_process_running(Process) ->
+	Result = [ X || X <- registered(), X =:= Process],
+	
+	Length = length(Result),
+	if 
+		Length == 0 -> false;
+		Length == 1 -> true;
+		Length > 1 -> throw({oops,"Muliple init processes"})
+	end.
+
+
+
+%% Register a process (it should appear in the list of registered processes afterwards)
+my_register() ->
+
+	Process = my_proc_name,
+	R = is_process_running(Process),
+	if 
+		R == true -> throw(already_running_ooops);
+		R == false -> io:fwrite("Not running - okay~n")
+	end,
+	
+	Pid = spawn(fun myproc2/0),
+	register(Process,Pid),
+	
+	R2 = is_process_running(Process),
+	if 
+		R2 == true -> io:fwrite("Process is running~n");
+		R2 == false -> throw(not_running)
+	end,
+	R2.
+
+
+%% Another proc that waits and exits
+myproc2() ->
+	timer:sleep(500),
+	exit('Done').  % Note the apostrophies around the atom owing to the capital D
+
 
--- Language/test/first_process_tests.erl
+++ Language/test/first_process_tests.erl
@@ -28,7 +28,21 @@ do_link1_test() ->
 do_link2_test() ->
 	?assertEqual(ok,first_process:do_link2()).
 	
-	
+%% If this fails it returns a message in a tuple rather than a reference in the form #Ref<1.2.3.4>
+monitor_you_test() ->
+	?assert(is_reference(first_process:monitor_you())).
+
+registered_test() ->
+	?assert(is_list(first_process:my_registered())).
+
+is_init_process_running_test() ->
+	?assert(first_process:is_init_process_running()).
+
+is_init_process_running2_test() ->
+	?assert(first_process:is_init_process_running2()).
+
+my_register_test() ->
+	?assert(first_process:my_register()).
 
 %% ====================================================================
 %% Internal functions
