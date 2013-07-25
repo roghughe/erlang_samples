51ffd4190c1550ecb94f37d7a2bb2bdf27542e12
Roger Hughes <me@rogerhughes.co.uk>
2013-07-25 14:19:55 +0100

    Added Overview.edoc file and changed application type to 'library'.

--- Language/src/language.erl
+++ Language/src/language.erl
@@ -18,26 +18,22 @@
 
 %% start/2
 %% ====================================================================
-%% @doc <a href="http://www.erlang.org/doc/apps/kernel/application.html#Module:start-2">application:start/2</a>
+%% @doc This project has been written as a library application. Hence, start/2 doesn't do anything.
+%% See <a href="http://www.erlang.org/doc/apps/kernel/application.html#Module:start-2">application:start/2</a>
 -spec start(Type :: normal | {takeover, Node} | {failover, Node}, Args :: term()) ->
 	{ok, Pid :: pid()}
 	| {ok, Pid :: pid(), State :: term()}
 	| {error, Reason :: term()}.
 %% ====================================================================
-start(Type, StartArgs) ->
-    case 'TopSupervisor':start_link(StartArgs) of
-		{ok, Pid} ->
-			{ok, Pid};
-		Error ->
-			Error
-    end.
+start(_Type, _StartArgs) ->
+	ok.
 
 %% stop/1
 %% ====================================================================
 %% @doc <a href="http://www.erlang.org/doc/apps/kernel/application.html#Module:stop-1">application:stop/1</a>
 -spec stop(State :: term()) ->  Any :: term().
 %% ====================================================================
-stop(State) ->
+stop(_State) ->
     ok.
 
 %% ====================================================================
