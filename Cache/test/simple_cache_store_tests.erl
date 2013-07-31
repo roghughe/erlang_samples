e2987ffc943f4d2864f4824cb79b035ed0710481
Roger Hughes <me@rogerhughes.co.uk>
2013-07-31 16:07:11 +0100

    Added incomplete simple_cache_store tests

---
+++ Cache/test/simple_cache_store_tests.erl
@@ -0,0 +1,19 @@
+%% @author Roger
+%% @doc Tests for simple_cache_store (which encapsulates ETS tables as part of its implementation)
+
+-include_lib("eunit/include/eunit.hrl").
+
+-module(simple_cache_store_tests).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
