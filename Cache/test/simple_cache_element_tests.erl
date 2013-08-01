e3175241fa4bef2fa97ed20e0d3c01645f952129
Roger Hughes <me@rogerhughes.co.uk>
2013-08-01 15:54:54 +0100

    Added additional unit tests

--- Cache/test/simple_cache_element_tests.erl
+++ Cache/test/simple_cache_element_tests.erl
@@ -8,6 +8,8 @@
 -define(RETURN,{ok,2500}).
 -define(VALUE,"value").
 -define(LEASETIME,3000).
+-define(STARTTIME,1000).
+-define(TIMELEFT,0).
 
 %% ====================================================================
 %% API functions
@@ -16,7 +18,7 @@
 
 %% @doc Example of how to use meck (this isn't the best way see: <a href="http://labs.mochimedia.com/archive/2011/06/13/meck-eunit-best-practices/">meck and eunit best practices</a>)
 %% Note:  this throws an exception as gen_server is a sticky module (i.e. a module located in a directory that's marked as sticky and cannot be reloaded)
-start_link_test() ->
+start_link() -> %% commented out...  _test() ->
     try
 	    meck:new(gen_server),
     	meck:expect(gen_server, % module
@@ -34,10 +36,36 @@ start_link_test() ->
     end.
 
 
+%% Returns the initial State of the gen_server.  
+%% The state record: state, {value,lease_time,start_time}
+init_test_() ->
+	{ok,{state,Value,LeaseTime,_StartTime},TimeLeft} = simple_cache_element:init([?VALUE,?LEASETIME]),
+	[?_assertEqual(?VALUE,Value),
+     ?_assertEqual(3000,LeaseTime),
+	 ?_assertEqual(3000000,TimeLeft)].
 
-init_test() ->
-	simple_cache_element:init([?VALUE,?LEASETIME]).
-	
+
+handle_call_for_fetch_test_() ->
+	ExpectedState = {state,?VALUE,?LEASETIME,?STARTTIME},
+	{reply, {ok,Value}, State,TimeLeft} = simple_cache_element:handle_call(fetch, from_unused, ExpectedState),
+	[?_assertEqual(?VALUE,Value),
+     ?_assertEqual(ExpectedState,State),
+     ?_assertEqual(?TIMELEFT,TimeLeft)].
+
+
+handle_cast_for_replace_test_() ->
+	ExpectedState = {state,?VALUE,?LEASETIME,?STARTTIME},
+	{noreply, {ok,Value}, State,TimeLeft} = simple_cache_element:handle_cast({replace,?VALUE},ExpectedState),
+	[?_assertEqual(?VALUE,Value),
+	 ?_assertEqual(ExpectedState,State),
+	 ?_assertEqual(?TIMELEFT,TimeLeft)].
+
+handle_cast_for_delete_test_() ->
+	ExpectedState = {state,?VALUE,?LEASETIME,?STARTTIME},
+	{stop, normal, State} = simple_cache_element:handle_cast(delete,ExpectedState),
+	[?_assertEqual(ExpectedState,State)].
+
+
 
 
 %% ====================================================================
---
+++ Cache/test/simple_cache_sup_tests.erl
@@ -0,0 +1,27 @@
+%% @author Roger
+%% @doc Demonstres the return tuple from an supervisor init/1 callback
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+-module(simple_cache_sup_tests).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+
+%% Demonstrates the return tuple / required for a supervisor init method 
+init_test() ->
+    Result = simple_cache_sup:init([]),
+	Children = [{simple_cache_element, {simple_cache_element,start_link,[]},
+			   temporary,brutal_kill,worker,[simple_cache_element]}],
+	RestartStrategy = {simple_one_for_one,0,1},
+	?assertEqual({ok,{RestartStrategy,Children}},Result).
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
